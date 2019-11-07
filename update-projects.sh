f8e087cbe16efc32aa03421649bdf3281b9abadc
Flávio Oliveira <flaoliveira@uolinc.com>
2019-11-07 08:37:39 -0300

    atualizando projeto users e inclusão de script para build de projetos

---
+++ update-projects.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+WORKDIR=$(pwd)
+
+echo $WORKDIR
+
+echo "Building netflix-gateway"
+cd $WORKDIR/netflix-gateway
+mvn package
+
+echo "Building netflix-gateway"
+cd $WORKDIR/netflix-gateway
+mvn package
+
+echo "Building netflix-user"
+cd $WORKDIR/netflix-user
+mvn package
